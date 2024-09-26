6019724b1a8ddcfd013ab4b54a282d3cc2b0b783
Anthony <ant.davi07@gmail.com>
2024-09-25 22:33:01 -0300

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,65 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, InfiniteScrollCustomEvent, IonList, IonItem, IonAvatar, IonSkeletonText, IonAlert } from '@ionic/angular/standalone';
+import { MovieService } from '../services/movie.service';
+import { catchError, finalize } from 'rxjs';
+import { MovieResult } from '../services/interfaces';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonAlert, IonSkeletonText, IonAvatar, IonItem, IonList, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  private movieService = new MovieService();
+  private currentPage = 1;
+  public error = null;
+  public isLoading = false;
+  private movies: MovieResult[] = [];
+  public imageBaseUrl = 'https://image.tmdb.org/t/p/';
+  public dummyArray = new Array(5);
+
+  constructor() {
+    this.loadMovies();
+  }
+
+  loadMovies(event?: InfiniteScrollCustomEvent) {
+    this.error = null
+
+    if (!event) {
+      this.isLoading = true;
+    }
+
+    this.movieService
+      .getTopRatedMovies(this.currentPage)
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+          if (event) {
+            event.target.complete();
+          }
+        }),
+        catchError((err: any) => {
+          console.log(err);
+          this.error = err.error.status_message;
+          return [];
+        })
+      )
+      .subscribe({
+        next: (res) => {
+          console.log(res)
+
+          this.movies.push(...res.results);
+
+          if (event) {
+            event.target.disabled = res.total_pages === this.currentPage;
+          }
+        }
+      })
+  }
+
+  loadMore(event: InfiniteScrollCustomEvent) {
+
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '9b2dbb75acbf1d9f7ae2c41a278e48c2',
 };
 
 /*
